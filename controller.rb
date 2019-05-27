4fca0497f80cf44fc0a5ae6eb12b1b477095683c
dafyddllyr <dafyddllyr@gmail.com>
2019-05-27 20:55:47 +0100

    Functionality updated and complete

--- controller.rb
+++ controller.rb
@@ -7,13 +7,17 @@ get '/' do
   erb(:home)
 end
 
-get "/:player_1" do
-  @player_1 = params[:player_1]
-  erb(:holding_page)
+get "/play" do
+  erb(:player_1)
 end
 
-get "/:first/:second" do
-  game = Game.new(params[:first], params[:second])
+get "/play/:player_1" do
+  @player_1 = params[:player_1]
+  erb(:player_2)
+end
+
+get "/play/:player_1/:player_2" do
+  game = Game.new(params[:player_1], params[:player_2])
   @result = game.play()
-  erb(:game)
+  erb(:result)
 end
