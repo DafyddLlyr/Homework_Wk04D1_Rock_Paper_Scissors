c45a9dfe26be30cfb9d476709b2f7d6914f3a8a1
dafyddllyr <dafyddllyr@gmail.com>
2019-05-27 23:06:01 +0100

    Testing heroku

--- controller.rb
+++ controller.rb
@@ -3,6 +3,8 @@ require("sinatra/contrib/all")
 require_relative("models/game")
 also_reload("models/*")
 
+class Controller < Sinatra::Base
+
 get '/' do
   erb(:home)
 end
@@ -20,4 +22,6 @@ get "/play/:player_1/:player_2" do
   game = Game.new(params[:player_1], params[:player_2])
   @result = game.play()
   erb(:result)
+end
+
 end
